c7c1d3c953c723aafeab6bbffa7a575cbae14c79
1114079140 <32088142+1114079140@users.noreply.github.com>
2023-12-31 13:05:54 +0800

    Update defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -37,8 +37,8 @@
 
 #define DEF_WLAN_2G_CC		"CN"
 #define DEF_WLAN_5G_CC		"US"
-#define DEF_WLAN_2G_SSID	BOARD_PID "test"
-#define DEF_WLAN_5G_SSID	BOARD_PID "5G"
+#define DEF_WLAN_2G_SSID	"test"
+#define DEF_WLAN_5G_SSID	"5G"
 #define DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"
 #define DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"
 #define DEF_WLAN_2G_PSK		"12345678"
